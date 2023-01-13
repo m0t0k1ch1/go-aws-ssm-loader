1686fea3da6958ec7144d42c9deec0699ba9ca05
m0t0k1ch1 <m0t0k1ch1.310@gmail.com>
2023-01-13 18:32:47 +0900

    feat: implemented

--- loader.go
+++ loader.go
@@ -1,17 +1,33 @@
 package ssmloader
 
 import (
-	"fmt"
+	"context"
 	"reflect"
+	"strings"
 
 	"github.com/pkg/errors"
+
+	"github.com/aws/aws-sdk-go-v2/aws"
+	aws_ssm "github.com/aws/aws-sdk-go-v2/service/ssm"
 )
 
 const (
 	tagKey = "ssm"
 )
 
-func Load(v any) error {
+type Loader struct {
+	awsConfig aws.Config
+}
+
+func NewLoader(awsConf aws.Config) *Loader {
+	return &Loader{
+		awsConfig: awsConf,
+	}
+}
+
+func (l *Loader) Load(ctx context.Context, v any) error {
+	ssm := aws_ssm.NewFromConfig(l.awsConfig)
+
 	rv := reflect.ValueOf(v)
 	if rv.Kind() != reflect.Pointer || rv.IsNil() {
 		return errors.New("v must be a pointer of a struct")
@@ -29,8 +45,40 @@ func Load(v any) error {
 		keys[i] = rt.Field(i).Tag.Get(tagKey)
 	}
 
-	// WIP
-	fmt.Println(keys)
+	params := map[string]string{}
+	{
+		var names []string
+		for _, key := range keys {
+			if len(key) == 0 {
+				continue
+			}
+
+			names = append(names, key)
+		}
+
+		out, err := ssm.GetParameters(ctx, &aws_ssm.GetParametersInput{
+			Names:          names,
+			WithDecryption: aws.Bool(true),
+		})
+		if err != nil {
+			return errors.Wrap(err, "failed to get SSM parameters")
+		}
+		if len(out.InvalidParameters) > 0 {
+			return errors.Errorf("invalid params: %s", strings.Join(out.InvalidParameters, ","))
+		}
+
+		for _, param := range out.Parameters {
+			params[*param.Name] = *param.Value
+		}
+	}
+
+	for i, key := range keys {
+		if len(key) == 0 {
+			continue
+		}
+
+		rv.Field(i).SetString(params[key])
+	}
 
 	return nil
 }
--- loader_test.go
+++ loader_test.go
@@ -1,17 +1,29 @@
 package ssmloader
 
 import (
+	"context"
 	"testing"
 
+	aws_config "github.com/aws/aws-sdk-go-v2/config"
 	"github.com/google/go-cmp/cmp"
 )
 
 type TestConfig struct {
 	Field1 string `ssm:"TestString"`
-	Field2 string `ssm:"TestSecureString"`
+	Field2 string
+	Field3 string `ssm:"TestSecureString"`
 }
 
 func TestLoad(t *testing.T) {
+	ctx := context.Background()
+
+	awsConf, err := aws_config.LoadDefaultConfig(ctx, aws_config.WithRegion("ap-northeast-1"))
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	l := NewLoader(awsConf)
+
 	t.Run("failure", func(t *testing.T) {
 		tcs := []struct {
 			Name         string
@@ -33,11 +45,18 @@ func TestLoad(t *testing.T) {
 				TestConfig{},
 				"v must be a pointer of a struct",
 			},
+			{
+				"struct pointer with invalid tag",
+				&struct {
+					Field string `ssm:"InvalidString"`
+				}{},
+				"invalid params: InvalidString",
+			},
 		}
 
 		for _, tc := range tcs {
 			t.Run(tc.Name, func(t *testing.T) {
-				err := Load(tc.Input)
+				err := l.Load(ctx, tc.Input)
 				if err == nil {
 					t.Errorf("err must not be nil")
 					return
@@ -52,11 +71,11 @@ func TestLoad(t *testing.T) {
 	t.Run("success", func(t *testing.T) {
 		out := TestConfig{
 			Field1: "TS",
-			Field2: "TSS",
+			Field3: "TSS",
 		}
 
 		var conf TestConfig
-		if err := Load(&conf); err != nil {
+		if err := l.Load(ctx, &conf); err != nil {
 			t.Fatal(err)
 		}
 
